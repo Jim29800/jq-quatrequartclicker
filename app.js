47542e0658fb7a03a9721294eacd4bcc618f93bf
Jim29800 <jbouvier1173@gmail.com>
2017-08-01 10:58:45 +0200

    initial

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	//votre code
-
-
+	var count = 0;
+	var message = [
+	" allez tuer des pinguins par exemple",
+	" supprimer ubuntu et installer un windows",
+	" faire un pile ou face gagnant avec votre voisin"
+	]
+	$(".cake").click(function () {
+		count++;
+		var alea = Math.floor(Math.random() * message.length);
+		//console.log(alea)
+		$(".counter").html(count)
+		if (count%20 === 0) {
+						alert("Trouvez une activité plus productive: " + message[alea])		
+		}
+	})
 });
